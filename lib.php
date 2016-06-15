085e47cfd005acc6eaefe3adaca0912d75ca6530
Aaron Wells <aaronw@catalyst.net.nz>
2016-06-15 19:26:28 +1200

    Creating lib.php file as needed

---
+++ lib.php
@@ -0,0 +1,14 @@
+<?php
+
+function mahara_add_instance($newmodule){}
+function mahara_update_instance($newmodule){}
+function mahara_delete_instance($id){}
+function mahara_user_complete($course, $user, $mod, $newmodule){}
+function mahara_user_outline(){}
+function mahara_print_recent_activity($course, $isteacher, $timestart){}
+function mahara_cron(){}
+// This is just a placeholder function that shouldn't actually add
+// any activity types
+function mahara_get_types() {
+    return array();
+}
